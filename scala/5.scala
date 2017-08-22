b6c18f0e60732f4a445c2d29dfccd6cf9213a924
chigichan24 <kazuki.chigichan@gmail.com>
2017-08-22 23:21:34 +0900

    Q5をとりあえず

--- scala/5.scala
+++ scala/5.scala
@@ -1,3 +1,14 @@
-def passStudents(scores: Map[String, Map[String, Int]]): Map[String, Int] ={
-	
+def passStudents(scores: Map[String, Map[String, Int]]):Map[String, Int]  = {
+	val keys = Set("math","english")
+	val result = collection.mutable.Map[String,Int]()
+	for(
+		v <- scores
+	){
+		val fetched = v._2.filterKeys(keys)
+		if(fetched.size == 2){
+			val average = fetched.map(_._2).sum/fetched.size
+			result += v._1 -> average
+		}
+	}
+	result.filter(_._2>=80).toMap
 }
