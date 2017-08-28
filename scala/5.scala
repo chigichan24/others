c382158b66fbd8b07f2694e55aea129c16753d26
chigichan24 <kazuki.chigichan@gmail.com>
2017-08-28 20:52:13 +0900

    いい感じにした．

--- scala/5.scala
+++ scala/5.scala
@@ -1,14 +1,14 @@
-def passStudents(scores: Map[String, Map[String, Int]]):Map[String, Int]  = {
-	val keys = Set("math","english")
+def passStudents(scores: Map[String, Map[String, Int]]):Map[String, Int] = {
 	val result = collection.mutable.Map[String,Int]()
 	for(
 		v <- scores
-	){
-		val fetched = v._2.filterKeys(keys)
-		if(fetched.size == 2){
-			val average = fetched.map(_._2).sum/fetched.size
-			result += v._1 -> average
+	){ 
+		val average = for(
+			t <- v._2.get("math")
+		)yield{
+			for(s <- v._2.get("english"))yield{(s+t)/2}
 		}
+		for(p<-average){for(r<-p){result += v._1 -> r}}
 	}
 	result.filter(_._2>=80).toMap
 }
