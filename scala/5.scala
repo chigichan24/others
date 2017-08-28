662c9c890ce60f73779807787fb2ca456aabd224
chigichan24 <kazuki.chigichan@gmail.com>
2017-08-28 22:51:15 +0900

    綺麗にした

--- scala/5.scala
+++ scala/5.scala
@@ -3,12 +3,13 @@ def passStudents(scores: Map[String, Map[String, Int]]):Map[String, Int] = {
 	for(
 		v <- scores
 	){ 
-		val average = for(
-			t <- v._2.get("math")
-		)yield{
-			for(s <- v._2.get("english"))yield{(s+t)/2}
+		for{
+			math <- v._2.get("math")
+			english <- v._2.get("english")
 		}
-		for(p<-average){for(r<-p){result += v._1 -> r}}
+		yield{
+			result += v._1 -> (math+english)/2
+		}
 	}
 	result.filter(_._2>=80).toMap
 }
