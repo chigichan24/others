3ef176820a66718b22eca920f3e24b5393bd69f8
chigichan24 <kazuki.chigichan@gmail.com>
2017-08-29 00:37:27 +0900

    綺麗に書いた

--- scala/5.scala
+++ scala/5.scala
@@ -1,14 +1,10 @@
 def passStudents(scores: Map[String, Map[String, Int]]):Map[String, Int] = {
-	val result = collection.mutable.Map[String,Int]()
-	scores.foreach{
-		val math = v._2.get("math")
-		val english = v._2.get("english")
-		for{
-			m <- math
-			e <- english
-		}{
-			result += v._1 -> (m+e)/2
-		}
+	for{
+		v <- scores
+		m <- v._2.get("math")
+		e <- v._2.get("english")
+		if (m+e)/2>=80
+	}yield{
+		v._1 -> (m+e)/2
 	}
-	result.filter(_._2>=80).toMap
 }
